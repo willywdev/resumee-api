b047010d89f1f5d105a338f134a939ce094fbbc3
Wilhelm Werwein <werwein.dev@gmail.com>
2023-10-18 09:03:26 +0200

    write a simple server

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	print("Hello World!")
+	handleRequest()
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	print("Welcome!\n")
+	print("Endpoint hit: Home\n")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(":10000", nil))
 }
