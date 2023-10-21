233ed5a31c6cca7285e32c957f4d25adae44efdb
Wilhelm Werwein <werwein.dev@gmail.com>
2023-10-21 11:51:04 +0200

    edit tsconfig and add json response

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import Bao from "baojs";
+import info from "./info.json";
 
 const app = new Bao();
 const port = parseInt(process.env.PORT || "4545");
 
 app.get("/", (ctx) => {
-  return ctx.sendJson({ message: "Hello World!" });
+  return ctx.sendJson(info);
 });
 
 const server = app.listen({ port: port });
