2ed0568467e634ead904e23e999e4dcd5663aa00
Wilhelm Werwein <werwein.dev@gmail.com>
2023-10-21 12:16:35 +0200

    add index.html and serve skills.json

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import Bao from "baojs";
 import info from "./data/info.json";
-import serveStatic from "serve-static-bun";
+import skills from "./data/skills.json";
 
 const app = new Bao();
 const port = parseInt(process.env.PORT || "4545");
 
-app.get("/", () => {
-  return serveStatic("./static/index.html");
-});
-
 app.get("/info", (ctx) => {
   return ctx.sendJson(info);
+});
+
+app.get("/skills", (ctx) => {
+  return ctx.sendPrettyJson(skills);
 });
 
 const server = app.listen({ port: port });
