c0471d1401d10beb4164548c65bd784bc3371071
Wilhelm Werwein <werwein.dev@gmail.com>
2023-10-21 11:20:07 +0200

    rm rf

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Elysia } from "elysia";
-
-const server = new Elysia().get("/", () => "Hello!").listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${server.server?.hostname}:${server.server?.port}`
-);
